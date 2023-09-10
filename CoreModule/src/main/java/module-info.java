e7cb26e9691f797d9e1b2b40bec5f60ceb31951a
Minh Vương <hardingadonis@gmail.com>
2023-09-10 13:19:44 +0700

    Add Log4J to Core Module

--- CoreModule/src/main/java/module-info.java
+++ CoreModule/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module pezhi.core {
     requires static lombok;
 
     requires com.google.gson;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
 
     exports utils;
+
+    opens utils to com.google.gson;
 }
