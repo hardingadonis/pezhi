aae296fe909d8f6d7e18f69c9d01745f8cbc61f2
Minh Vương <hardingadonis@gmail.com>
2023-09-11 20:13:10 +0700

    Add Log4J and JavaFx in UI Module

--- UIModule/src/main/java/module-info.java
+++ UIModule/src/main/java/module-info.java
@@ -1,3 +1,8 @@
 module pezhi.ui {
     requires pezhi.core;
+
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
+    requires javafx.controls;
+    requires javafx.fxml;
 }
